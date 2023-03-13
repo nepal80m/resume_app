939146e771861fa037e43d2dbfa88b1915011f5f
Asim Nepal <gnepal80m@gmail.com>
2023-03-13 17:27:02 +0545

    Completed the app

---
+++ lib/const.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+// Text colors
+const cTextHeaderColor = Color(0xff2A2A2A);
+const cTextFeatureColor = Color(0xff707070);
+const cTextLabelColor = Colors.white;
+
+// Detail for profile item
+const cProfileTopic = 'About Me';
+const cProfileFeature =
+    "Ideator, Tutor, and Full Stack Developer. Skilled in C, C++, Python, Django, Flutter, Blockchain Technologies. Pursuing BE in Computer Engineering from IOE, TU.";
+const cProfileLabel = 'Profile';
+const cProfilePicture = 'assets/images/avatar.png';
+const cProfileIconImage = 'assets/images/profile.png';
+const cProfileColor = Color(0xff845EC2);
+const cProfileBegColor = Color(0xff845EC2);
+const cProfileEndColor = Color(0xffB8A1DC);
+
+// Detail for education item
+const cEducationTopic = 'Educational Background';
+const cEducationFeature =
+    'Bachelor Degree in Computer Science\n\n Lalitpur Engineering College\n' +
+        '2018 - 2023';
+const cEducationLabel = 'Education';
+const cEducationPicture = 'assets/images/book.png';
+const cEducationIconImage = 'assets/images/education.png';
+const cEducationColor = Color(0xffFFC75F);
+const cEduBegColor = Color(0xffFFC75F);
+const cEduEndColor = Color(0xffF3DDB2);
+
+// Detail for experience item
+const cExperienceTopic = 'Work Experience';
+const cExperienceFeature = 'Software Engineer \nKhalti (Nov.2022-Present)\n\n' +
+    ' Full Stack Developer \nAfsoTech (Jun. 2020 to Nov. 2022)\n\n' +
+    ' Full Stack Developer \nTriphala Cooperative (Mar. 2020 to May. 2020)\n';
+const cExperienceLabel = 'Experience';
+const cExperiencePicture = 'assets/images/laptop.png';
+const cExperienceIconImage = 'assets/images/experience.png';
+const cExperienceColor = Color(0xffFF5E78);
+const cExpBegColor = Color(0xffF8C8D0);
+const cExpEndColor = Color(0xffFF5E78);
+
+final cCaptionTextSize = 20.0.sp;
+const cStartingIndex = 0;
